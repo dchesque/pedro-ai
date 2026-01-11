1695987590ac2a7379a4bf46c5ec5cdee99cbe5f
Driano Chesque <dchesque@gmail.com>
2026-01-11 00:24:24 -0300

    fix: resolve db.short undefined by bypassing Windows file lock on Prisma generator

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../prisma/generated/client";
+import { PrismaClient } from "../../prisma/generated/client_new";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
